08651a9897821e3d0bf9133d92011662958326d2
Rituparna Matkar <rituparna89@gmail.com>
2015-08-30 22:12:17 +0530

    add site layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = 'Home'
   end
 
   def contact
+    @title = 'Contact'
   end
+
+  def about
+        @title = 'Aboout Us'
+  end
+
 end
